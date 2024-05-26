0c2eaf165df8499a12c5644fbf24976cfd7047ad
OneLiL05 <kirillofficial0507@gmail.com>
2024-05-27 01:04:58 +0300

    refactor: refactor Update action

--- Utilities/Actions/UpdateAction.cs
+++ Utilities/Actions/UpdateAction.cs
@@ -4,9 +4,11 @@ namespace trade_compas.Utilities.Actions;
 
 public class UpdateAction<TEntity>
 {
+    private readonly GetAllAction<TEntity> _getAllAction = new();
+
     public void DoAction(string collectionPath, Predicate<TEntity> match, Action<TEntity> updateAction)
     {
-        var list = FileHelper.LoadData<TEntity>(collectionPath);
+        var list = _getAllAction.DoAction(collectionPath);
 
         list
             .Where(entity => match(entity))
